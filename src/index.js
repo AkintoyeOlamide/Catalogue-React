758c01184eb398f3c087720647e400ae14f0e8af
Akintoyeolamide <Akintolamide500@gmail.com>
2021-08-15 21:24:00 +0100

    finished working on project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from 'react-redux';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import configureStore from './configureStore';
+
+const state = {
+  categories: [
+    {
+      strCategory: 'Pork',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/pork.png',
+    },
+    {
+      strCategory: 'Vegetarian',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/vegetarian.png',
+    },
+    {
+      strCategory: 'Beef',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/beef.png',
+    },
+    {
+
+      strCategory: 'Chicken',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/chicken.png',
+    },
+    {
+      strCategory: 'Dessert',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/dessert.png',
+    },
+    {
+      strCategory: 'Lamb',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/lamb.png',
+    },
+    {
+      strCategory: 'Miscellaneous',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/miscellaneous.png',
+      strCategoryDescription: "General foods that don't fit into another category",
+    },
+    {
+      strCategory: 'Pasta',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/pasta.png',
+    },
+    {
+      strCategory: 'Side',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/side.png',
+    },
+    {
+      strCategory: 'Starter',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/starter.png',
+    },
+    {
+      strCategory: 'Vegan',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/vegan.png',
+    },
+    {
+      strCategory: 'Goat',
+      strCategoryThumb: 'https://www.themealdb.com/images/category/goat.png',
+    },
+  ],
+};
+
+const store = configureStore(state);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
