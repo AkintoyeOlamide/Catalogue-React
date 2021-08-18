c789abb9830ae8fce0791d5ec07177420d59c288
Akintoyeolamide <Akintolamide500@gmail.com>
2021-08-18 08:56:08 +0100

    modularize the filter function

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,18 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import '../index.css';
 
 const Header = ({ categories }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-">
+  <nav className="navbar navbar-expand-lg navbar-light bg-dark">
     <div className="container">
-      <span className="head-text navbar-brand text-white pg-title" href="#">
-        Richies Recipe
-      </span>
-      <button
-        className="navbar-toggler text-white"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <span className="navbar-brand text-white pg-title" href="#">Catalogue of Recipes</span>
+      <button className="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon text-white" />
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to="/" className="nav-link text-white">
-              Home
-            </Link>
+            <Link to="/" className="nav-link text-white">Home</Link>
           </li>
           <li className="nav-item dropdown">
             <span
@@ -42,14 +29,12 @@ const Header = ({ categories }) => (
             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
               {categories.map((category) => (
                 <li key={category.strCategory}>
-                  <Link
-                    to={`/meals/${category.strCategory}`}
-                    className="dropdown-item"
-                  >
+                  <Link to={`/meals/${category.strCategory}`} className="dropdown-item">
                     {category.strCategory}
                   </Link>
                 </li>
               ))}
+
             </ul>
           </li>
         </ul>
